3d4b6800eac7f145982751b774b6db1f3e1e7774
MikeJongenVention <mike.jongen@vention.nl>
2020-12-01 09:09:08 +0100

    Added three number functionality.

--- src/day-1-report-repair.cr
+++ src/day-1-report-repair.cr
@@ -1,12 +1,16 @@
 require "option_parser"
 
 file_name = ""
+triple = false
 
 OptionParser.parse do |parser|
   parser.banner = "Welcome to Report Repair"
 
   parser.on "-f FILE", "--file=FILE", "Input file" do |file|
     file_name = file
+  end
+  parser.on "-t", "--three", "Use three Numbers" do
+    triple = true
   end
   parser.on "-h", "--help", "Show help" do
     puts parser
@@ -20,12 +24,26 @@ unless file_name.empty?
   values_str.each {|x| values_int << x.to_i}
   
   index = 0
-  values_int[0,values_str.size].each do |y|
-    values_int[index,values_str.size].each do |x|
-      if (y + x) == 2020
-        puts y * x
+  if triple
+    index2 = 0
+    values_int[0,values_str.size].each do |y|
+      values_int[index,values_str.size].each do |x|
+        values_int[index2,values_str.size].each do |z|
+          if (y + x + z) == 2020
+            puts y * x * z
+          end
+        end
       end
+      index += 1
     end
-    index += 1
+  else
+    values_int[0,values_str.size].each do |y|
+      values_int[index,values_str.size].each do |x|
+        if (y + x) == 2020
+          puts y * x
+        end
+      end
+      index += 1
+    end
   end
 end
