6baa070eec19882025801d9d3dc1d01a7d5ef01d
MikeJongenVention <mike.jongen@vention.nl>
2020-12-01 07:55:40 +0100

    get filename

--- src/day-1-report-repair.cr
+++ src/day-1-report-repair.cr
@@ -1,6 +1,19 @@
-# TODO: Write documentation for `Day::1::Report::Repair`
-module Day::1::Report::Repair
-  VERSION = "0.1.0"
+require "option_parser"
 
-  # TODO: Put your code here
+file_name = ""
+
+OptionParser.parse do |parser|
+  parser.banner = "Welcome to Report Repair"
+
+  parser.on "-f FILE", "--file=FILE", "Input file" do |file|
+    file_name = file
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+end
+
+unless file_name.empty?
+  puts "filename: #{file_name}"
 end
